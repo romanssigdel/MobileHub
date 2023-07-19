354e65cf5879fc4de4fd403b37830ee738c23520
Romans Sigdel <romanssigdel8@gmail.com>
2023-07-19 12:48:53 +0545

    data fetch for containers of the products

--- user/index.php
+++ user/index.php
@@ -7,9 +7,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mobilehub</title>
     <link rel="shortcut icon" href="image/icon.png">
-    <link rel="stylesheet" href="../css/style.css">
-    <link rel="stylesheet" href="../css/utils.css">
-    <link rel="stylesheet" href="../css/responsive.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/utils.css">
+    <link rel="stylesheet" href="css/responsive.css">
 </head>
 
 <script src="../js/script.js"></script>
@@ -18,7 +18,11 @@
 <script src="https://kit.fontawesome.com/4eca1fe67d.js" crossorigin="anonymous"></script>
 
 <body>
-    <?php include "header.php";?>
+    <!-- Imported header file -->
+    <?php 
+    include "header.php";
+     ?>
+
 
     <main>
         <div class="container">
@@ -28,36 +32,45 @@
                 </div>
                 <div class="toolbar-product_items">
                     <div class="toolbar">
-                        <h3>Smartphones(0)</h3>
+                        <h3>Smartphones</h3>
                     </div>
                     <div class="product-items" id="content">
-                        <div class="product-item">
-                            <div class="product-item-info">
-                                <a href="#">
-                                    <span class="product-image-container" style="width: 240px;">
-                                        <span class="product-image-wrapper">
-                                            <img class="product-image-photo" src="../1.png" max-width="240" max-height="300" alt="">
-                                        </span>
-                                    </span>
-                                </a>
-                                <div class="product-details">
-                                    <div class="product-item-name">
-                                        <a class="product-item-link" href="#" onclick="loadContent('home.html');return false;">Samsung Galaxy M52-5g
+
+                        <!-- Database connection and fetching the data-->
+                        <?php include "config.php";
+                        $query = mysqli_query($con, "SELECT * FROM `tblproduct`");
+                        while ($row = mysqli_fetch_array($query)) {
+                            echo "
+                                <div class='product-item'>
+                                    <div class='product-item-info'>
+                                        <a href='#'>
+                                            <span class='product-image-container' style='width: 240px;'>
+                                                <span class='product-image-wrapper'>
+                                                    <img class='product-image-photo' src='../admin/product/$row[Pimage]' max-width='240' max-height='300'>
+                                                </span>
+                                            </span>
                                         </a>
+                                        <div class='product-details'>
+                                            <div class='product-item-name'>
+                                                <a class='product-item-link' href='#' onclick='loadContent('home.php');return false;'>$row[Pname]
+                                                </a>
+                                            </div>
+                                            <div class='price-review'>
+                                                <span class='price'>$row[Pprice]</span>
+                                                <span class='review-summary'>
+                                                    <img src='../img/star-icon.png' alt=''>
+                                                    <span class='rating-result'>4.2</span>
+                                                </span>
+                                            </div>
+                                            <span class='arrives'>Arrives: </span>
+                                            <span class='two-days'>Within 2 days</span>
+                                        </div>
                                     </div>
-                                    <div class="price-review">
-                                        <span class="price">Rs.40000</span>
-                                        <span class="review-summary">
-                                            <img src="../img/star-icon.png" alt="">
-                                            <span class="rating-result">4.2</span>
-                                        </span>
-                                    </div>
-                                    <span class="arrives">Arrives: </span>
-                                    <span class="two-days">Within 2 days</span>
                                 </div>
-                            </div>
-                        </div>
-                        <div class="product-item">
+                                ";
+                        }
+                        ?>
+                        <!-- <div class="product-item">
                             <div class="product-item-info">
                                 <a href="#">
                                     <span class="product-image-container">
@@ -82,13 +95,12 @@
                                     <span class="two-days">Within 2 days</span>
                                 </div>
                             </div>
-                        </div>
+                        </div> -->
                     </div>
                 </div>
             </div>
         </div>
     </main>
-    <div style="width: 240vh; height:100vh;"></div>
     <footer class="flex-all-center">
         <p>Copyright &copy; Mycart.com</p>
     </footer>
