301053fb5aa9fdca556227c1a33c50e5d5d2519b
Romans Sigdel <romanssigdel8@gmail.com>
2023-07-19 16:19:53 +0545

    user interface

--- user/index.php
+++ user/index.php
@@ -25,41 +25,44 @@
 
 
     <main>
-        <div class="container">
-            <div class="sidebar-toolbar-product_items">
+        <div class="container" id="content">
+            <div class="sidebar-toolbar-product_items" >
                 <div class="menu">
                     <h3>Categories</h3>
                 </div>
-                <div class="toolbar-product_items">
+                <div class="toolbar-product_items" >
                     <div class="toolbar">
                         <h3>Smartphones</h3>
                     </div>
-                    <div class="product-items" id="content">
+                    <div class="product-items" >
 
                         <!-- Database connection and fetching the data-->
                         <?php include "config.php";
                         $query = mysqli_query($con, "SELECT * FROM `tblproduct`");
-                        while ($row = mysqli_fetch_array($query)) {
-                            echo "
-                                <div class='product-item'>
-                                    <div class='product-item-info'>
-                                        <a href='#'>
-                                            <span class='product-image-container' style='width: 240px;'>
-                                                <span class='product-image-wrapper'>
-                                                    <img class='product-image-photo' src='../admin/product/$row[Pimage]' max-width='240' max-height='300'>
+                        $check = mysqli_num_rows($query)>0;
+
+                        if($check){
+                            while($row = mysqli_fetch_assoc($query)){
+                                ?>
+                                <div class="product-item">
+                                    <div class="product-item-info">
+                                        <a href="#">
+                                            <span class="product-image-container" style="width: 240px;">
+                                                <span class="product-image-wrapper">
+                                                    <img class="product-image-photo" src="../admin/product/<?php echo $row['Pimage']; ?>" max-width="240" max-height="300">
                                                 </span>
                                             </span>
                                         </a>
-                                        <div class='product-details'>
-                                            <div class='product-item-name'>
-                                                <a class='product-item-link' href='#' onclick='loadContent('home.php');return false;'>$row[Pname]
+                                        <div class="product-details">
+                                            <div class="product-item-name">
+                                                <a class="product-item-link" href="home.php" onclick="loadContent('description_page.php');return false;"><?php echo $row['Pname']; ?>
                                                 </a>
                                             </div>
-                                            <div class='price-review'>
-                                                <span class='price'>$row[Pprice]</span>
-                                                <span class='review-summary'>
+                                            <div class="price-review">
+                                                <span class="price"><?php echo $row['Pprice']; ?></span>
+                                                <span class="review-summary">
                                                     <img src='../img/star-icon.png' alt=''>
-                                                    <span class='rating-result'>4.2</span>
+                                                    <span class="rating-result">4.2</span>
                                                 </span>
                                             </div>
                                             <span class='arrives'>Arrives: </span>
@@ -67,9 +70,13 @@
                                         </div>
                                     </div>
                                 </div>
-                                ";
+                                <?php
+                            }
+                        }else{
+                            echo "nothing";
                         }
-                        ?>
+                        // while ($row = mysqli_fetch_array($query)) {
+                            ?>
                     </div>
                 </div>
             </div>
