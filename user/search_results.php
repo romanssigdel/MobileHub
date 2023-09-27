239f68dea1fbd5b9edf9f06317bd3a0bafeadd07
Romans Sigdel <romanssigdel8@gmail.com>
2023-09-27 16:23:23 +0545

    search

--- user/search_results.php
+++ user/search_results.php
@@ -75,7 +75,25 @@
 
     <!-- Include the footer -->
     <footer class="flex-all-center">
-        <p>Copyright &copy; Mycart.com</p>
+        <div class="aboutus-contact-copyright">
+            <div class="copyright">
+                <p>Copyright &copy; Mobilehub.com</p>
+            </div>
+            <div class="aboutus">
+                <p>Welcome to Mobilehub! A place to find the best mobile products.
+            Mobilehub is the home to many of mobiles that you can possibly imagine—from trending companies like Apple, Samsung, Google.
+            Just search for what you're looking for, and you'll be sure to find it here.
+            Whats more?
+            We are bringing the products at irresistible prices, a hassle-free return policy, and quick delivery with free shipping on orders above Rs. 5000.
+            So, what are you waiting for? Drag your cursor to the search bar, enter the product, and place your order at the cheapest price.</p>
+            </div>
+            <div class="contact">
+                <p>Details</p>
+                <p>Banepa-10, Kavrepalanchok,Bagmati Province, Nepal</p>
+                <p>mobilehubnp@gmail.com</p>
+                <p>+977 9841324578, 001-23435</p>
+            </div>
+        </div>
     </footer>
 </body>
 
