3d9e176850e8304f65295470cd4f98af0f3cd69b
Romans Sigdel <romanssigdel8@gmail.com>
2023-09-28 21:06:36 +0545

    api

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 require ('stripe-php-master/init.php');
 
-$Publishablekey = "pk_test_51NvE3BBJfYGwNsoj7K7iy4ljWLAwqpFGW5KjjwvtD3pv4Yx2Irrkk4DNuMOlGRxor4XKlWnZ7UPc1ISBxIUuAwXs00wZfYXrEV";
+$Publishablekey = "";
 $Secretkey = "";
 
 \Stripe\Stripe::setApiKey($Secretkey);
