4728b515005eacfaccc1b8ea5fe31adf9c13abe5
Romans Sigdel <romanssigdel8@gmail.com>
2023-10-04 17:10:51 +0545

    admin product

--- admin/product/index.php
+++ admin/product/index.php
@@ -7,10 +7,38 @@
     <title>Product-page</title>
     <!-- BootStrap CDN -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/4eca1fe67d.js" crossorigin="anonymous"></script>
 </head>
 
 <body>
-    <?php include "../mystore.php"?>
+<?php
+  session_start();
+  if(!$_SESSION['admin']){
+    header('location: form/login.php');
+    exit();
+  }
+?>
+<nav class="navbar  bg-dark">
+  <div class="container-fluid text-white">
+    <a class="navbar-brand text-white">My Store</a>
+    <span>
+    <i class="fas fa-user-shield"></i>
+        <?php echo ucwords($_SESSION['admin']);?> |
+        <i class="fa-solid fa-right-to-bracket"></i>
+    <a href="../form/logout.php" class="text-decoration-none text-white">Logout</a> |
+    <a href="../../user/index.php" class="text-decoration-none text-white">Userpanel</a>
+    </span>
+  </div>
+</nav>
+
+<div>
+    <h2 class="text-center">Dashboard</h2>
+</div>
+<div class="col-md-6 bg-danger text-center m-auto">
+    <a href="index.php" class="text-white text-decoration-none fs-4 fw-bold px-5 py-5 ">Add Product</a>
+    <a href="../user.php" class="text-white text-decoration-none fs-4 fw-bold px-5 py-5" >Users</a>
+</div>
+
     <div class="container">
         <div class="row">
             <div class="col-md-6 m-auto border border-primary mt-3">
