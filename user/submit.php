94197bf294338f694a69a91f85deea8554bcff4a
Romans Sigdel <romanssigdel8@gmail.com>
2023-10-08 16:32:24 +0545

    payment charge

--- user/submit.php
+++ user/submit.php
@@ -1,35 +1,4 @@
 <?php
-
-// use Stripe\Terminal\Location;
-
-// require("../config.php");
-
-// if(isset($_POST['stripeToken']))
-// \Stripe\Stripe:: setVerifySslCerts(false);
-
-// $token = $_POST['stripeToken'];
-// $amount = $_POST['amount'];
-
-
-
-// $data = \Stripe\Charge::create(array(
-//     "amount"=>intval($amount)*100,
-//     "currency"=>"inr",
-//     "description"=> "Bought",
-//     "source"=> $token,
-// ));
-
-// echo "<pre>";
-// if($data){
-//     echo "
-//     <script>
-//     alert('Successfully Transaction completed');
-//     window.location.href='index.php';
-//     </script>";
-// }
-
-?>
-<?php
 session_start();
 use Stripe\Terminal\Location;
 
