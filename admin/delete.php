6541ddcba2a1c3b3dcb2a7d1f840c59e00cdf6b1
Romans Sigdel <romanssigdel8@gmail.com>
2023-10-13 14:22:11 +0545

    ch

--- admin/delete.php
+++ admin/delete.php
@@ -3,6 +3,13 @@ $Id = $_GET['id'];
 
 # Connecting to database and fetching the user table.
 $con = mysqli_connect('localhost', 'root', '', 'mobilehub');
-mysqli_query($con,"DELETE FROM `tbluser` WHERE Id ='$Id'");
-header("location:user.php");
+$query = mysqli_query($con,"DELETE FROM `tbluser` WHERE Id ='$Id'");
+if ($query){
+    echo "
+        <script>
+            alert('Successfully deleted.');
+            window.location.href='user.php';
+        </script>";
+}
+
 ?>
